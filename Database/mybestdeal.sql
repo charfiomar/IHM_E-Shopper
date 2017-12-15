64bce197a2d0da898f51c2c062baa869823eb713
charfiomar <omar.charfi@iit.ens.tn>
2017-12-15 16:56:49 +0100

    database modified

--- Database/mybestdeal.sql
+++ Database/mybestdeal.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  localhost
--- Généré le :  Lun 04 Décembre 2017 à 14:35
+-- Généré le :  Ven 15 Décembre 2017 à 16:56
 -- Version du serveur :  5.7.20-0ubuntu0.16.04.1
 -- Version de PHP :  7.0.22-0ubuntu0.16.04.1
 
@@ -30,10 +30,11 @@ CREATE TABLE `annonce` (
   `idAnn` int(11) NOT NULL,
   `titreAnn` varchar(100) NOT NULL,
   `prixAnn` float NOT NULL,
+  `unitAnn` float NOT NULL,
   `descriptionAnn` varchar(500) NOT NULL,
   `dateCreAnn` datetime NOT NULL,
   `dateExpAnn` datetime NOT NULL,
-  `etatAnn` enum('Neuf','Occasion') NOT NULL,
+  `etatAnn` enum('New','Used') NOT NULL,
   `idUsr` int(11) NOT NULL,
   `idCat` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Details annonce';
@@ -42,8 +43,16 @@ CREATE TABLE `annonce` (
 -- Contenu de la table `annonce`
 --
 
-INSERT INTO `annonce` (`idAnn`, `titreAnn`, `prixAnn`, `descriptionAnn`, `dateCreAnn`, `dateExpAnn`, `etatAnn`, `idUsr`, `idCat`) VALUES
-(1, 'Leatherman Squirt ps4 Bleu Mini Outil', 10.8, 'Le vendeur assume l\'entière responsabilité de cette annonce.', '2017-12-04 12:58:37', '2017-12-04 12:58:37', 'Neuf', 1, 2);
+INSERT INTO `annonce` (`idAnn`, `titreAnn`, `prixAnn`, `unitAnn`, `descriptionAnn`, `dateCreAnn`, `dateExpAnn`, `etatAnn`, `idUsr`, `idCat`) VALUES
+(5, 'MacBook Pro 2017', 500, 25, '13" MacBook Pro, Touch Bar, Space Grey, 3,1 GhZ, 512GB SSD, wie neu', '2017-12-12 00:00:00', '2017-12-15 00:00:00', 'Used', 1, 1),
+(7, ' Apple iPhone X', 900, 45, 'Apple-iPhone-X-256GB-Space-Grey-034-FACTORY-UNLOCKED-034-from-Australia-NEW', '2017-12-12 00:00:00', '2017-12-16 00:00:00', 'New', 1, 1),
+(8, 'Assorted Crimp Spade Terminal', 3.2, 0.15, '280pcs Assorted Crimp Spade Terminal Insulated Electrical Wire Connector Kit Set', '2017-12-12 00:00:00', '2017-12-14 00:00:00', 'New', 1, 2),
+(9, 'Car Rear View Reverse Backup Camera', 10, 0.5, 'Night Vision Waterproof Car Rear View Reverse Backup Camera 170°CMOS Anti Fog U#', '2017-12-12 00:00:00', '2017-12-26 00:00:00', 'New', 1, 6),
+(10, 'CHEMISE TUNIQUE', 7, 0.3, 'TAILLE-42-AU-52-CHEMISE-TUNIQUE-BLOUSE-FEMME-NOIR-BLANC-GRANDES-TAILLES', '2017-12-12 00:00:00', '2017-12-15 00:00:00', 'Used', 3, 4),
+(11, 'Training bike', 115, 5, 'Training indoor bike Kettler Axos Cycle P Gray', '2017-12-12 00:00:00', '2017-12-18 00:00:00', 'New', 4, 7),
+(12, 'Acoustic guitar', 30, 1.5, 'SUPERBE GUITARE ACOUSTIQUE FENDER +HOUSSE +ACCORDEUR +SANGLES A VOIR', '2017-12-12 00:00:00', '2017-12-29 00:00:00', 'Used', 3, 3),
+(13, 'Sony PS4', 245, 10, 'PS4 Pro Noire 1 To + Gran Turismo Sport', '2017-12-13 00:00:00', '2017-12-31 00:00:00', 'New', 6, 8),
+(14, 'Mike Oldfield', 10, 0.25, 'Mike Oldfield La Collection Musique Album CD', '2017-12-15 00:00:00', '2017-12-30 00:00:00', 'Used', 10, 5);
 
 -- --------------------------------------------------------
 
@@ -64,7 +73,12 @@ CREATE TABLE `categorie` (
 INSERT INTO `categorie` (`idCat`, `nomCat`, `desCat`) VALUES
 (1, 'Technology', 'Technology in all its forms'),
 (2, 'DIY', 'Do-it-Yourself tools'),
-(3, 'LifeStyle', 'LifeStyle tools');
+(3, 'LifeStyle', 'LifeStyle tools'),
+(4, 'Fashion', 'All about fashion'),
+(5, 'Collections', 'Antiques and collections'),
+(6, 'Auto-Moto', 'Cars and motorcycles'),
+(7, 'Sports', 'All about Sports'),
+(8, 'Entertainment', 'All about Entertainment');
 
 -- --------------------------------------------------------
 
@@ -75,9 +89,24 @@ INSERT INTO `categorie` (`idCat`, `nomCat`, `desCat`) VALUES
 CREATE TABLE `enchere` (
   `idEn` int(11) NOT NULL,
   `sommeEn` float NOT NULL,
+  `dateEn` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `idUsr` int(11) NOT NULL,
   `idAnn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Contenu de la table `enchere`
+--
+
+INSERT INTO `enchere` (`idEn`, `sommeEn`, `dateEn`, `idUsr`, `idAnn`) VALUES
+(1, 6.3, '2017-12-15 16:31:41', 1, 10),
+(2, 7, '2017-12-15 16:32:41', 1, 10),
+(3, 210, '2017-12-15 16:33:41', 1, 13),
+(4, 3.2, '2017-12-15 16:31:41', 10, 8),
+(5, 235, '2017-12-15 16:44:50', 10, 13),
+(6, 245, '2017-12-15 16:45:50', 10, 13),
+(7, 115, '2017-12-15 16:47:53', 10, 11),
+(8, 10, '2017-12-15 16:48:33', 1, 14);
 
 -- --------------------------------------------------------
 
@@ -99,11 +128,15 @@ INSERT INTO `favoris` (`idUsr`, `idCat`) VALUES
 (5, 1),
 (6, 1),
 (7, 1),
+(10, 1),
 (5, 2),
 (7, 2),
 (5, 3),
 (6, 3),
-(7, 3);
+(7, 3),
+(10, 4),
+(10, 6),
+(9, 8);
 
 -- --------------------------------------------------------
 
@@ -146,7 +179,6 @@ INSERT INTO `pays` (`idPays`, `nomPays`) VALUES
 CREATE TABLE `photo` (
   `idPh` int(11) NOT NULL,
   `nomPh` varchar(250) NOT NULL,
-  `extensionPh` varchar(4) NOT NULL,
   `descPh` varchar(250) NOT NULL,
   `idAnn` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -155,8 +187,16 @@ CREATE TABLE `photo` (
 -- Contenu de la table `photo`
 --
 
-INSERT INTO `photo` (`idPh`, `nomPh`, `extensionPh`, `descPh`, `idAnn`) VALUES
-(1, 's-l1600', 'jpg', 'Leatherman Squirt ps4 Bleu Mini Outil', 1);
+INSERT INTO `photo` (`idPh`, `nomPh`, `descPh`, `idAnn`) VALUES
+(4, '/phppSxXnY', 'MacBook Pro 2017', 5),
+(6, '/phpJDIkju', ' Apple iPhone X', 7),
+(7, '/phpjBcx1m', 'Assorted Crimp Spade Terminal', 8),
+(8, '/phphG4VOn', 'Car Rear View Reverse Backup Camera', 9),
+(9, '/phpluIdVE', 'CHEMISE TUNIQUE', 10),
+(10, '/phpmT2PJE', 'Training bike', 11),
+(11, '/phpDZxa2E', 'Acoustic guitar', 12),
+(12, '/phpwkBAnj', 'Sony PS4', 13),
+(13, '/phpaS5cqA', 'Mike Oldfield', 14);
 
 -- --------------------------------------------------------
 
@@ -193,10 +233,15 @@ CREATE TABLE `utilisateur` (
 --
 
 INSERT INTO `utilisateur` (`idUsr`, `loginUsr`, `mdpsUsr`, `mailUsr`) VALUES
+(1, 'omarcharfi', 'Omar180794', 'omar.charfi@iit.ens.tn'),
+(2, 'charfiomar85', 'Omar180794', 'omarcharfi85@gmail.com'),
 (3, 'HajjejMaha', 'Mitcha52', 'maha.hajjej@iit.ens.tn'),
 (4, 'Anonymous12', '147258', 'myMail@yahoo.fr'),
 (5, 'oommoo', 'Omar1111', 'oo@ii.aa'),
-(7, 'Maha', 'Mitcha52', 'mahahajjej4@gmail.com');
+(6, 'Hamouda', 'katout1', 'hamouda125.fh@gmail.com'),
+(7, 'Maha', 'Mitcha52', 'mahahajjej4@gmail.com'),
+(9, 'E-shopper', '123456', 'new_user@gmail.com'),
+(10, 'boss', '123456', 'faiez162@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -304,17 +349,17 @@ ALTER TABLE `ville`
 -- AUTO_INCREMENT pour la table `annonce`
 --
 ALTER TABLE `annonce`
-  MODIFY `idAnn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idAnn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 --
 -- AUTO_INCREMENT pour la table `categorie`
 --
 ALTER TABLE `categorie`
-  MODIFY `idCat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idCat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT pour la table `enchere`
 --
 ALTER TABLE `enchere`
-  MODIFY `idEn` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idEn` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT pour la table `pays`
 --
@@ -324,7 +369,7 @@ ALTER TABLE `pays`
 -- AUTO_INCREMENT pour la table `photo`
 --
 ALTER TABLE `photo`
-  MODIFY `idPh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `idPh` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 --
 -- AUTO_INCREMENT pour la table `recherche`
 --
@@ -334,7 +379,7 @@ ALTER TABLE `recherche`
 -- AUTO_INCREMENT pour la table `utilisateur`
 --
 ALTER TABLE `utilisateur`
-  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `idUsr` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 --
 -- AUTO_INCREMENT pour la table `ville`
 --
